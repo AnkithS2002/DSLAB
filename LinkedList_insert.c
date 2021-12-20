9eb20ae432f9d8da75bb2dc59e76ea1f5172c105
Ankith S <92366708+AnkithS2002@users.noreply.github.com>
2021-12-20 21:39:13 +0530

    Update LinkedList_insert.c

--- LinkedList_insert.c
+++ LinkedList_insert.c
@@ -27,21 +27,82 @@ void append(struct Node* *head_ref, int new_data){
     last->next = new_node;
     return;
 }
+void insert_pos(struct Node* *head_ref, int new_data, int position){
+    struct Node *new_node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* temp = *head_ref;
+    struct Node* temp1 = *head_ref;
+    int count = 0;
+    while (temp1->next != NULL)
+    {
+        count++;
+        temp1 = temp1->next;
+    }
+    new_node->data = new_data;
+    if(position == 1){
+        new_node->next = temp;
+        *head_ref = new_node;
+        printf("Inserted %d at position %d..\n", new_data, position);
+        return;
+    }
+    if(position > 1 && position <=count){
+        for (int i = 1; i < position - 1; i++){
+        temp = temp->next;
+        }
+        new_node->next = temp->next;
+        temp->next = new_node;
+        printf("Inserted %d at position %d..\n", new_data, position);
+    }
+    else{
+        printf("Invalid position..\n");
+    }
+    
+}
 
 void printList(struct Node *node){
     while (node != NULL)
     {
-        printf("%d ", node->data);
+        printf("%d ->", node->data);
         node = node->next;
     }
 }
 int main(){
     struct Node *head = NULL;
-    append(&head, 6);
-    push(&head, 7);
-    push(&head, 1);
-    append(&head, 4);
-    printf("Created linked list is: ");
-    printList(head);
+    int choice, num, pos;
+    do{
+        printf("1.INSERT AT BEGINNING\n2.INSERT AT END\n3.INSERT AT POSITION\n4.DISPLAY\n5.EXIT : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the no. to insert: ");
+            scanf("%d", &num);
+            push(&head, num);
+            printf("Inserted %d at the beginning..\n",num);
+            break;
+        case 2:
+            printf("Enter the no. to insert: ");
+            scanf("%d", &num);
+            append(&head, num);
+            printf("Inserted %d at the end..\n",num);
+            break;
+        case 3:
+            printf("Enter the no. to insert: ");
+            scanf("%d", &num);
+            printf("Enter the position: ");
+            scanf("%d", &pos);
+            insert_pos(&head, num, pos);
+            break;
+        case 4:
+            printf("Created linked list is: ");
+            printList(head);
+            printf("NULL\n");
+            break;
+        case 5:
+            break;
+        default:
+            printf("Invalid input! Try again...\n");
+            break;
+        }        
+    } while (choice != 5);
     return 0;
 }
